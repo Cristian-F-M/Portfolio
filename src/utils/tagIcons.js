c4a3c6927e0a37017a14d168ed6fb896ee9dd923
Cristian Morales <cfmorales.diaz@gmail.com>
2024-04-26 20:36:17 -0500

    Update tagIcons.js module

--- src/utils/tagIcons.js
+++ src/utils/tagIcons.js
@@ -4,6 +4,16 @@ import MySQLIcon from "@/icons/MySQL.astro";
 import PostgreSQLIcon from "@/icons/PostgreSQL.astro";
 import SassIcon from "@/icons/Sass.astro";
 import HtmlIcon from "@/icons/Html.astro";
+import CssIcon from "@/icons/Css.astro";
+import Tailwind from "@/icons/Tailwind.astro";
+import AstroIcon from "@/icons/AstroIcon.astro";
+import FlaskIcon from "@/icons/Flask.astro";
+import PHPIcon from "@/icons/PHP.astro";
+import LaravelIcon from "@/icons/Laravel.astro";
+import JavaIcon from "@/icons/Java.astro";
+import SQLiteIcon from "@/icons/SQLite.astro";
+import GitIcon from "@/icons/Git.astro";
+import GitHubIcon from "@/icons/GitHub.astro";
 
 
 export const tagIcons = {
@@ -13,4 +23,14 @@ export const tagIcons = {
     PostgreSQL: { icon: PostgreSQLIcon, iconClass: "size-5" },
     Sass: { icon: SassIcon, iconClass: "size-6" },
     Html: { icon: HtmlIcon, iconClass: "size-5" },
+    Css: { icon: CssIcon, iconClass: "size-5" },
+    Tailwind: { icon: Tailwind, iconClass: "size-5" },
+    Astro: {icon: AstroIcon, iconClass: "size-5"},
+    Flask : {icon: FlaskIcon, iconClass: "size-5"},
+    PHP : {icon: PHPIcon, iconClass: "size-5"},
+    Laravel : {icon: LaravelIcon, iconClass: "size-5"},
+    Java : {icon: JavaIcon, iconClass: "size-5" },
+    SQLite : {icon: SQLiteIcon, iconClass: "size-7"},
+    Git: {icon: GitIcon, iconClass: "size-5"},
+    GitHub: {icon: GitHubIcon, iconClass: "size-5"},  
 };
