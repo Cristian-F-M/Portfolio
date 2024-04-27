90c87e62d2efb478188ccdd911d3fb0c7f4a7d68
Cristian Morales <cfmorales.diaz@gmail.com>
2024-04-26 19:19:06 -0500

    Add tagIcons.js module and update TagBentoItem.astro to use it

---
+++ src/utils/tagIcons.js
@@ -0,0 +1,16 @@
+import PythonIcon from "@/icons/Python.astro";
+import JavaScriptIcon from "@/icons/JavaScript.astro";
+import MySQLIcon from "@/icons/MySQL.astro";
+import PostgreSQLIcon from "@/icons/PostgreSQL.astro";
+import SassIcon from "@/icons/Sass.astro";
+import HtmlIcon from "@/icons/Html.astro";
+
+
+export const tagIcons = {
+    Python: { icon: PythonIcon, iconClass: "size-7" },
+    JavaScript: { icon: JavaScriptIcon, iconClass: "size-4" },
+    MySQL: { icon: MySQLIcon, iconClass: "size-6" },
+    PostgreSQL: { icon: PostgreSQLIcon, iconClass: "size-5" },
+    Sass: { icon: SassIcon, iconClass: "size-6" },
+    Html: { icon: HtmlIcon, iconClass: "size-5" },
+};
