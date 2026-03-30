92fe399c498638ab80a618d30f7d538d03e18aef
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-03-30 13:12:07 -0500

    Add output, integrations and adapter to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-      plugins: [tailwindcss()]
+	output: 'server',
+	vite: {
+		plugins: [tailwindcss()]
 	},
-
-  integrations: [react()]
+	integrations: [react()],
+	adapter: vercel()
 });
