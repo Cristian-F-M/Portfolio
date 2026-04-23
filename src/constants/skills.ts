0a0dabef9ab2d9f34a23ada1957c9e34562bccc7
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-04-22 21:09:44 -0500

    Use dinamic values instead of hard coded values

---
+++ src/constants/skills.ts
@@ -0,0 +1,7 @@
+export const FRONTEND = ['react', 'tailwind_css', 'next.js', 'css', 'typescript']
+export const CORE = ['typescript', 'node_js', 'sql', 'tailwindcss']
+export const MOBILE = ['react', 'react_native']
+
+
+
+
