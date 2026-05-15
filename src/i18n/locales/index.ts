f758d701ee3d911f11786eb90c88e9fdb1402ac0
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-05-14 19:38:49 -0500

    Remove useless locale files

--- src/i18n/locales/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as es } from './es';
-export { default as en } from './en';
-export { default as fr } from './fr';
-export { default as pr } from './pr';
-export { default as cn } from './cn';
