22957fbe10257161bbbd6c3ac7426941d1424d90
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-05-14 19:41:16 -0500

    Use router provider and import i18n

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { RouterProvider } from 'react-router/dom'
+import Router from '@/routes'
 
 import '@/src/styles/global.css'
+import "@/i18n";
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<App />
+		<RouterProvider router={Router} />
 	</StrictMode>
 )
