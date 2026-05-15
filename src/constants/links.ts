66b6d466181f438bc1c2e8970ec98b58ae611b51
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-05-14 19:41:37 -0500

    Add icon to links constants

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,30 +1,42 @@
 import type { defaultLangKeys } from '@/types/i18n'
+import {
+	IconCode,
+	IconHome,
+	IconMail,
+	IconUserCircle,
+	type IconProps
+} from '@tabler/icons-react'
 
 export interface Link {
 	id: string
 	url: string
 	path: keyof defaultLangKeys['index']['nav']['links']
+	icon: React.ComponentType<IconProps>
 }
 
 export const links = [
 	{
 		id: 'home',
 		path: 'home',
-		url: '#home'
+		url: '#home',
+		icon: IconHome
 	},
 	{
 		id: 'projects',
 		path: 'projects',
-		url: '#projects'
+		url: '#projects',
+		icon: IconCode
 	},
 	{
 		id: 'me',
 		path: 'me',
-		url: '#me'
+		url: '#me',
+		icon: IconUserCircle
 	},
 	{
 		id: 'contact',
 		path: 'contact',
-		url: '#contact'
+		url: '#contact',
+		icon: IconMail
 	}
 ] satisfies Link[]
