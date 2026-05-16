74b1e56a9b9d605bc65ce5796424850c8bedc857
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-05-16 17:29:49 -0500

    Fix achievements ids

--- src/constants/achievements.ts
+++ src/constants/achievements.ts
@@ -1,5 +1,5 @@
 export const ACHIEVEMENTS = [
-	{ id: '', value: '+2', path: 'years_experience' },
-	{ id: '', value: '25+ ', path: 'projects_done' },
-	{ id: '', value: '15 ', path: 'technologies_learned' }
+	{ id: 'years_experience', value: '+2', path: 'years_experience' },
+	{ id: 'projects_done', value: '25+ ', path: 'projects_done' },
+	{ id: 'technologies_learned', value: '15 ', path: 'technologies_learned' }
 ]
