998398c3a5b9764bb0e4753055ba662845fd8dd2
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-05-16 14:59:28 -0500

    Add achievements constants

---
+++ src/constants/achievements.ts
@@ -0,0 +1,5 @@
+export const ACHIEVEMENTS = [
+	{ id: '', value: '+2', path: 'years_experience' },
+	{ id: '', value: '25+ ', path: 'projects_done' },
+	{ id: '', value: '15 ', path: 'technologies_learned' }
+]
