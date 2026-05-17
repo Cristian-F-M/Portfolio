ea15d51e89cebf5222f34c75aece915a8d08ccc4
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-05-17 15:48:24 -0500

    Add social networks to me constant

--- src/constants/me.ts
+++ src/constants/me.ts
@@ -1,3 +1,32 @@
+import {
+	IconBrandGithub,
+	IconBrandLinkedin,
+	IconBrandX
+} from '@tabler/icons-react'
+
 export const ME = {
-	available_for_hire: true
+	available_for_hire: true,
+	networks: [
+		{
+			id: 'github',
+			text: 'GitHub',
+			url: 'https://github.com/Cristian-F-M',
+			user: '/Cristian-F-M',
+			icon: IconBrandGithub
+		},
+		{
+			id: 'linkedin',
+			text: 'Linkedin',
+			url: 'https://linkedin.com/in/cmoralesdev/',
+			user: '/cmoralesdev',
+			icon: IconBrandLinkedin
+		},
+		{
+			id: 'x',
+			text: 'x',
+			url: 'https://x.com/Morales_M20',
+			user: '/Morales_M20',
+			icon: IconBrandX
+		}
+	]
 }
