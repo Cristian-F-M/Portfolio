d66ad832e5ab89e1ee494b03c1abe76c31db8b76
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-05-18 14:11:05 -0500

    Remove unnecessary function and fix typing

--- src/state/language.ts
+++ src/state/language.ts
@@ -1,27 +1,15 @@
 import { create } from 'zustand'
-import { defaultLanguage } from '@/i18n/ui'
-import type { Languages } from '@/types/i18n'
+import { defaultLanguage } from '@/constants/i18n'
 
 interface LanguageState {
-	language: Languages
-	setLanguage: (language: Languages) => void
-	load: () => void
+	language: string
+	setLanguage: (language: string) => void
 }
 
-export const useLanguage = create<LanguageState>()((set, get) => ({
+export const useLanguage = create<LanguageState>()((set) => ({
 	language: defaultLanguage,
 	setLanguage: (language) => {
 		localStorage.setItem('language', language)
 		set({ language })
-	},
-	load: () => {
-		let lang = defaultLanguage
-		const url = window.location.pathname
-		const l = url.split('/')[1]
-
-		if (l) lang = l
-		else lang = localStorage.getItem('lang') ?? defaultLanguage
-
-		get().setLanguage(lang as Languages)
 	}
 }))
