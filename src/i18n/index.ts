08c4f9a289499f16cb732dab8eaee68d9be734a8
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-05-18 14:11:33 -0500

    Add supported languages to i18n configuration

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,12 +2,14 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import Backend from 'i18next-http-backend'
+import { supportedLanguages } from '@/constants/i18n'
 
 i18n
 	.use(LanguageDetector)
 	.use(Backend)
 	.use(initReactI18next)
 	.init({
+		supportedLngs: supportedLanguages,
 		detection: {
 			order: ['localStorage', 'navigator'],
 			caches: ['localStorage'],
