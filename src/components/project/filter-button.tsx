5179741d83a9e6b9054daafcfb7f843f7730ba7a
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-05-21 20:49:11 -0500

    Add button component to allow filter by projects types

---
+++ src/components/project/filter-button.tsx
@@ -0,0 +1,32 @@
+import { twMerge } from 'tailwind-merge'
+import Button from '../button'
+
+interface FilterButtonProps extends React.ComponentProps<'button'> {
+	isSelected: boolean
+	text: string
+	count: number
+}
+
+export default function FilterButton({
+	className,
+	isSelected,
+	text,
+	count,
+	...props
+}: FilterButtonProps) {
+	return (
+		<Button
+			variant={isSelected ? 'primary' : 'secondary'}
+			className={twMerge("py-2 px-3 text-xs uppercase font-jet-brains h-fit", isSelected && 'bg-primary/70 hover:bg-primary-hover/70')}
+			{...props}>
+			{text}{' '}
+			<span
+				className={twMerge(
+					'text-text-muted text-xs',
+					isSelected && 'text-text-primary'
+				)}>
+				({count})
+			</span>
+		</Button>
+	)
+}
