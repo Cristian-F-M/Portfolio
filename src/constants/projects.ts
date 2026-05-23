e8f8747028311eef7e14252111f038a0d51aa848
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-05-22 20:50:11 -0500

    Add more projects and their images

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -79,5 +79,106 @@ export const PROJECTS = [
 		type: 'web',
 		url: 'https://solitario-chi.vercel.app/',
 		active: true
+	},
+	{
+		id: 'vite-plugin-html-template',
+		name: 'vite-plugin-html-template',
+		images: ['1.avif'],
+		skills: [],
+		latest_version: '2.0.0',
+		release_year: '2026',
+		github: 'vite-plugin-html-template',
+		type: 'plugin',
+		url: 'https://www.npmjs.com/package/@cmorales_/vite-plugin-html-template',
+		active: true
+	},
+	{
+		id: 'web-votaciones-v2',
+		name: 'web-votaciones-v2',
+		images: ['1.png'],
+		skills: [],
+		github: 'web-votaciones-v2',
+		type: 'web',
+		active: false
+	},
+	{
+		id: 'fast-scroll-chrome-extension',
+		name: 'fast-scroll-chrome-extension',
+		images: ['1.png', '2.avif'],
+		skills: [],
+		github: 'fast-scroll-chrome-extension',
+		type: 'extension',
+		latest_version: '1.0.0',
+		release_year: '2026',
+		active: true
+	},
+	{
+		id: 'api-votaciones-v2',
+		name: 'api-votaciones-v2',
+		images: ['1.png', '2.png', '3.png', '4.png'],
+		skills: [],
+		github: 'api-votaciones-v2',
+		type: 'api',
+		active: false
+	},
+	{
+		id: 'python-games-web',
+		name: 'python-games-web',
+		images: ['1.png', '2.png', '3.png'],
+		skills: [],
+		github: 'python-games-web',
+		type: 'web',
+		active: true,
+		latest_version: '1.0.1',
+		release_year: '2025',
+		url: 'https://python-games-web.vercel.app'
+	},
+	{
+		id: 'wordle',
+		name: 'wordle',
+		images: ['1.png', '2.png', '3.png'],
+		skills: [],
+		github: 'wordle',
+		type: 'web',
+		active: true,
+		latest_version: '1.0.0',
+		release_year: '2025',
+		url: 'https://wordle-alpha-indol.vercel.app/'
+	},
+	{
+		id: 'buscaminas',
+		name: 'buscaminas',
+		images: ['1.png', '2.png', '3.png'],
+		skills: [],
+		type: 'web',
+		github: 'buscaminas',
+		active: true,
+		latest_version: '1.0.0',
+		release_year: '2025',
+		url: 'https://buscaminas-rho.vercel.app/'
+	},
+	{
+		id: 'video-player',
+		name: 'video-player',
+		images: ['1.avif'],
+		skills: [],
+		type: 'web',
+		github: 'video-player',
+		active: true,
+		latest_version: '1.0.0',
+		release_year: '2025',
+		url: 'https://video-player-lemon-six.vercel.app/'
+	},
+	{
+		id: 'astronomy-picture-day',
+		name: 'astronomy-picture-day',
+		images: ['1.png'],
+		skills: [],
+		type: 'web',
+		github: 'astronomy-picture-day',
+		active: true,
+		latest_version: '1.0.0',
+		release_year: '2025',
+		url: 'https://astronomy-picture-day.netlify.app/'
 	}
 ] satisfies Project[]
