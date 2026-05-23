df5a1d6b933f88ced5f4d3fc17158662df8f45fe
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-05-23 12:31:27 -0500

    Add sections to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import Home from '@/components/index/sections/home'
+import Me from '@/components/index/sections/me'
+import Contact from '@/components/index/sections/contact'
+import Projects from '@/components/index/sections/projects'
+
 function App() {
-	return
+	return (
+		<>
+			<Home />
+			<Projects />
+			<Me />
+			<Contact />
+		</>
+	)
 }
 
 export default App
